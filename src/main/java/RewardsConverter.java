63e979075afabcad444ff8bd0665163838bce5e6
Carlos Marin <carlosmarinphs@gmail.com>
2024-02-07 17:44:11 -0500

    I added the necessary code for this to be able to work

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -18,3 +18,30 @@ public class RewardsConverter {
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
+
+class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // This is the constructor for accepting the cash value
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // This is the constructor for aceepting the miles value
+
+    public RewardValue(int milesValue){
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    public double getCashValue(){
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
